ffa28e72e710f699a4b988c4eb0d2cac8c87ac81
Asmaa Elawady <asmaalwdy6@gmail.com>
2022-05-03 22:15:51 +0200

    initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+// FCI – Programming 1 – 2022 - Assignment 3
+// Program Name: .cpp // Last Modification Date: xx/xx/xxxx
+// Author1 and ID and Group: Salma ameer jalal, 20210505, A
+// Author2 and ID and Group: Marwa Sameh Taha , 20210514, A
+// Author3 and ID and Group:Asmaa Elawady Ibrahim,20210068,A
+// Teaching Assistant: Nesma mohamed
+
+
+
+
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <cmath>
+#include <fstream>
+
