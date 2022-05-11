7afe30a69c1fa6bacea385c9aaf08cdf79011dc2
salma ameer <salmaameer409@gmail.com>
2022-05-11 12:45:44 +0200

    add the headerfile

---
+++ functions.h
@@ -0,0 +1,19 @@
+//
+// Created by Salma ameer on 10/05/2022.
+//
+
+#ifndef ASS4_FUNCTIONS_H
+#define ASS4_FUNCTIONS_H
+
+#endif //ASS4_FUNCTIONS_H
+#include <string>
+using namespace std;
+
+
+struct user{
+    string email;
+    string password ;
+    string Id;
+    string name;
+
+};
